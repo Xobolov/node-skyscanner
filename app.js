1d874dc83ad286062259e3b6ed2d47edd6650cb3
Xobolov <farhad.xobolov@gmail.com>
2021-12-16 01:10:44 +0400

    Some more scraping attempts

--- app.js
+++ app.js
@@ -8,12 +8,20 @@ const url = 'https://www.skyscanner.net/';
     const context = await browser.newContext();
     const page = await context.newPage();
 
+    // Start tracing before creating / navigating a page.
+    await context.tracing.start({ screenshots: true, snapshots: true });
+
     await page.goto(url);
 
+    await clickInputs(page);
+
+    await browser.startTracing(page, {path: 'trace.json'});
 
     await page.locator('.BpkButtonBase_bpk-button__NTM4Y').click();
 
     await page.waitForNavigation();
+
+    await browser.stopTracing();
 
     let data = await page.evaluate(() => {
 
@@ -23,8 +31,30 @@ const url = 'https://www.skyscanner.net/';
 
     });
 
-    await browser.close();
+    // await browser.close();
 
     console.log(data);
 
+    // Stop tracing and export it into a zip archive.
+
+
+
+
 })();
+
+
+let clickInputs = async (page) => {
+
+    await page.click('#fsc-origin-search');
+    await page.fill('#fsc-origin-search', inputData.origin)
+
+    await page.click('#fsc-destination-search')
+    await page.fill('#fsc-destination-search', inputData.destination)
+}
+
+let inputData = {
+
+    origin: 'Baku',
+    destination: 'Istanbul',
+
+}
